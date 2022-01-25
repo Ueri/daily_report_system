0ae37c83312c29732ef73d564cf476bfb5de4208
yuri.shinonaga <yurishinonaga1229@outlook.jp>
2022-01-25 19:18:45 +0900

    Employeeモデル、EmployeeViewモデル作成

--- src/main/java/constants/AttributeConst.java
+++ src/main/java/constants/AttributeConst.java
@@ -35,6 +35,10 @@ public enum AttributeConst {
     ROLE_ADMIN(1),
     ROLE_GENERAL(0),
 
+    //削除フラグ
+    DEL_FLAG_TRUE(1),
+    DEL_FLAG_FALSE(0),
+
     // 日報管理
     REPORT("report"),
     REPORTS("reports"),
